f8e407696f60ae89ef98f07ebe316d3ea76b12ae
Brian Wilson <wilson@bubba.org>
2013-05-29 15:35:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+iPhotoDump
+=======
+
+Perl script that will dump/backup iPhoto/Aperture metadata
+
+This is helpful if you wish to leverage additional scripts to upload your
+files to 3rd party services (i.e flickr - http://www.panix.com/~eli/flickr/), 
+or simply backup your metadata. 
+
+
+Configuration 
+-------------
+Make sure you change the location of your iPhoto/Aperture database to reflect where it really is.  This will probably be "/Users/<you>/Pictures/iPhoto Library/Database/"
+
+
+Bugs/Contact Info
+-----------------
+Bug me on Twitter at [@brianwilson](http://twitter.com/brianwilson) or email me [here](http://cronological.com/comment.php?ref=bubba).
+
+
---
+++ iPhotoDump.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl
+#
+# Dump iPhoto/Aperture Info (Full Path/FileName/Name/Date/Size/Event/Description/Rating)
+#
+# This is helpful if you wish to leverage additional scripts to upload your
+# files to 3rd party services, or simply backup your metadata. Make sure you
+# change the location of your iPhoto/Aperture database to reflect where it
+# really is. 
+#
+# 5/29/2013 - bubba@bubba.org
+#
+# Fuzzing DB fun... 
+# To get Image data:
+# 
+# in RKVersion 'select modelId, fileName, name, projectUuid, mainRating from RKVersion';
+# 
+# To get Event data:
+# 
+# select uuid, name from RKFolder where uuid=projectUuid;
+#
+# Descriptions:
+# 
+# in Properties.apdb:
+# 
+# select versionId from RKIptcProperty where stringId=33927
+
+# select modelId, stringProperty from RKUniqueString 
+# 33927|Thursday - the calmest day of the week!
+# 
+
+use DBD::SQLite;
+use POSIX qw(strftime);
+
+$library_db="/tmp/Library.apdb";
+$prop_db   ="/tmp/Properties.apdb";
+
+# I made this shit up. just kidding.
+# http://www.ipadforums.net/ipad-help/84074-ipad-internal-date-format.html
+$unixOffset=978307200; 
+
+my $dbl = DBI->connect("dbi:SQLite:dbname=$library_db","","");
+
+my $database = "attach database \"$library_db\" as L";
+$dbl->do($database);
+my $database = "attach database \"$prop_db\" as P";
+$dbl->do($database);
+
+#$query ="select V.modelId, V.fileName, V.name, (select name from L.RKFolder as
+#F where V.projectUuid=F.uuid),(select stringProperty from P.RKUniqueString as U
+#where U.modelId=(select stringId from P.RKIptcProperty as I where
+#I.versionId=V.modelId and I.propertyKey='Caption/Abstract')), V.mainRating from
+#L.RKVersion as V where 1"; 
+$query = "select distinct(M.imagePath), M.imageDate, M.fileSize, V.fileName, V.name, (select name from L.RKFolder
+as F where V.projectUuid=F.uuid),(select stringProperty from P.RKUniqueString
+as U where U.modelId=(select stringId from P.RKIptcProperty as I where
+I.versionId=V.modelId and I.propertyKey='Caption/Abstract')), V.mainRating from
+L.RKVersion as V inner join L.RKMaster as M on V.masterUuid=M.uuid";
+
+my $stl = $dbl->prepare($query);
+$stl->execute();
+      
+while(my ($imagePath, $imagedate, $filesize, $filename, $name, $eventname, $description, $rating) = $stl->fetchrow()) {
+		$imagedate = strftime "%a %b %e %H:%M:%S %Y", localtime($imagedate+$unixOffset);
+		$items{"$imagePath"}{'imagedate'}=$imagedate;
+		$items{"$imagePath"}{'filesize'}=$filesize;
+		$items{"$imagePath"}{'filename'}=$filename;
+		$items{"$imagePath"}{'name'}=$name;
+		$items{"$imagePath"}{'event'}=$eventname;
+		if ($description) {
+			$items{"$imagePath"}{'description'}=$description;
+	    }
+		if ($rating > 0) { 
+			$items{"$imagePath"}{'rating'}=$rating;
+		}
+}
+
+
+foreach $item (sort keys %items) {
+	print "FilePath: $item\n";
+	foreach $val (keys %{$items{$item}}) {
+		print "\t$val => $items{$item}{$val}\n";
+	}
+}
+
