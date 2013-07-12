8e40b4aade5c1c0b4e05ebd85906ab8c334bf3a2
Brian Wilson <wilson@bubba.org>
2013-07-11 22:39:34 -0400

    Added keywords..

--- iPhotoDump.pl
+++ iPhotoDump.pl
@@ -30,16 +30,20 @@
 
 use DBD::SQLite;
 use POSIX qw(strftime);
+use DBD::mysql;
+use DateTime;
 
-$library_db="/tmp/Library.apdb";
-$prop_db   ="/tmp/Properties.apdb";
+$library_db="/Users/wilson/Pictures/iPhoto Library/Database/apdb/Library.apdb";
+$prop_db   ="/Users/wilson/Pictures/iPhoto Library/Database/apdb/Properties.apdb";
 
 # I made this shit up. just kidding.
 # http://www.ipadforums.net/ipad-help/84074-ipad-internal-date-format.html
 $unixOffset=978307200; 
 
-my $dbl = DBI->connect("dbi:SQLite:dbname=$library_db","","");
+# base dir of sycned library
+$base = "/Users/<user>/Pictures/iPhoto/Masters";
 
+my $dbl = DBI->connect("dbi:SQLite:dbname=$library_db","","");
 my $database = "attach database \"$library_db\" as L";
 $dbl->do($database);
 my $database = "attach database \"$prop_db\" as P";
@@ -50,20 +54,32 @@ $dbl->do($database);
 #where U.modelId=(select stringId from P.RKIptcProperty as I where
 #I.versionId=V.modelId and I.propertyKey='Caption/Abstract')), V.mainRating from
 #L.RKVersion as V where 1"; 
-$query = "select M.imagePath, V.imageDate, M.fileSize, V.fileName, V.name,
-(select name from L.RKFolder
+$query = "select V.modelId, M.imagePath, V.imageDate, M.fileSize, V.fileName, V.name, (select name from L.RKFolder
 as F where V.projectUuid=F.uuid),(select stringProperty from P.RKUniqueString
 as U where U.modelId=(select stringId from P.RKIptcProperty as I where
 I.versionId=V.modelId and I.propertyKey='Caption/Abstract')), V.mainRating from
 L.RKVersion as V inner join L.RKMaster as M on V.masterUuid=M.uuid order by
 V.imageDate";
 
+$keyword_query = "select K.name from RKKeyword as K join RKKeywordForVersion as
+KV on K.ModelId=KV.keywordId where KV.versionId=?";
+
 my $stl = $dbl->prepare($query);
 $stl->execute();
       
-while(my ($imagePath, $imagedate, $filesize, $filename, $name, $eventname, $description, $rating) = $stl->fetchrow()) {
-	        next if ($filename =~ /\d{10}\_[a-z0-9]+\_o/);  # ignore temporary? images created from flickr syncing process
-		$imagedate = strftime "%a %b %e %H:%M:%S %Y", localtime($imagedate+$unixOffset);
+while(my ($modelId, $imagePath, $imagedate, $filesize, $filename, $name, $eventname, $description, $rating) = $stl->fetchrow()) {
+
+	next if ($filename =~ /\d{10}\_[a-z0-9]+\_o/);  # ignore images created from flickr syncing process
+	next if ($filename =~ /\d{10}\.MP4/); # ignore movies created from flickr syncing process
+	#	$imagedate = strftime "%a %b %e %H:%M:%S %Y", localtime($imagedate+$unixOffset);
+		$stk = $dbl->prepare($keyword_query);
+		$stk->execute($modelId);
+		$keywords="";
+		while(my ($keyword) = $stk->fetchrow()) {
+			$keywords = $keywords . $keyword . ",";
+		}
+		chop($keywords);
+		$imagedate = strftime "%s", localtime($imagedate+$unixOffset);
 		$items{"$imagePath"}{'imagedate'}=$imagedate;
 		$items{"$imagePath"}{'filesize'}=$filesize;
 		$items{"$imagePath"}{'filename'}=$filename;
@@ -71,17 +87,40 @@ while(my ($imagePath, $imagedate, $filesize, $filename, $name, $eventname, $desc
 		$items{"$imagePath"}{'event'}=$eventname;
 		if ($description) {
 			$items{"$imagePath"}{'description'}=$description;
-	        }
+	    }
+		if ($keywords) {
+			$items{"$imagePath"}{'keywords'}=$keywords;
+		}
 		if ($rating > 0) { 
 			$items{"$imagePath"}{'rating'}=$rating;
 		}
 }
 
-
 foreach $item (sort keys %items) {
-	print "FilePath: $item\n";
-	foreach $val (keys %{$items{$item}}) {
-		print "\t$val => $items{$item}{$val}\n";
-	}
-}
+		$fullpath = $base . $item;
+		$file = $items{$item}{'filename'};
+		$album_title = $items{$item}{'event'};
+		$epoch = $items{$item}{'imagedate'};
+	    $description = $items{$item}{'description'};
+	    $rating = $items{$item}{'rating'};
+		$keyword = $items{$item}{'keywords'};
+		
+		if ($description =~ /^\s+$/) {
+			$description = "";
+		}
 
+		if ($file =~ /\.jpg|\.cr2|\.png/i) {
+			$is_image=1;
+		} else {
+			$is_image=0;
+		}
+
+        $dt = DateTime->from_epoch( epoch => $epoch );
+        $year   = $dt->year;
+        $month   = $dt->month;
+        if ($month < 10) {
+                $month = "0" . $month;
+        }
+
+		print "$fullpath | $file | $album_title | $epoch | $month | $year | $rating | $keyword | $description | $is_image\n";
+}
